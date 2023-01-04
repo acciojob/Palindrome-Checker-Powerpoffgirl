618ace3a59437d7561753d679b58e4625627c99f
Jyoti Singh <80877414+Powerpoffgirl@users.noreply.github.com>
2023-01-04 09:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-	let n = str.length;
-	for(let i=0; i<n; i++){
+	const n = str.length;
+	for(let i=0; i<n/2; i++){
 		if(str[i] != (str[n-1-i])){
 			return false;
 		}
